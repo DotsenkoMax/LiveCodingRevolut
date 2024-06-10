7da602f6eea575ae7c9642d0c5b1d1f9b639e4ca
alengul <alengul1@mail.ru>
2024-06-11 01:06:47 +0400

    Initial Empty Porject

---
+++ src/test/java/org/revolut/TestFromScratch.java
@@ -0,0 +1,15 @@
+package org.revolut;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TestFromScratch {
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+}
